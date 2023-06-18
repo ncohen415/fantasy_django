e25f4de13c3bd78ec02084cd820dd88aa67f78ea
ncohen415 <nate.cohen415@gmail.com>
2023-06-18 11:35:17 -0400

    ok

--- fantasy_django/frontend/components/Header.tsx
+++ fantasy_django/frontend/components/Header.tsx
@@ -1,9 +1,34 @@
 import React from "react"
+import { Box, Flex, Text, IconButton, Heading } from "@chakra-ui/react"
+import { HamburgerIcon } from "@chakra-ui/icons"
+import Link from "next/link"
+import { UserButton } from "@clerk/nextjs"
 
 type Props = {}
 
 const Header = (props: Props) => {
-  return <div>Header</div>
+  return (
+    <Box bg="gray.800" color="white" py={4} px={6}>
+      <Flex align="center" justify="space-between" mx="auto">
+        <Flex>
+          <Text fontSize="xl" fontWeight="bold">
+            My Website
+          </Text>
+          <Text mr={4}>Home</Text>
+          <Text mr={4}>About</Text>
+          <Text mr={4}>Services</Text>
+          <Text mr={4}>Contact</Text>
+        </Flex>
+        <Flex align="center" display={{ base: "none", md: "flex" }}>
+          <IconButton
+            aria-label="Toggle navigation"
+            icon={<HamburgerIcon />}
+            display={{ base: "block", md: "none" }}
+          />
+        </Flex>
+      </Flex>
+    </Box>
+  )
 }
 
 export default Header
--- fantasy_django/frontend/pages/_app.tsx
+++ fantasy_django/frontend/pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from "next/app"
 import { ClerkProvider } from "@clerk/nextjs"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 
 export default function App({ Component, pageProps }: AppProps) {
+  const colors = {
+    brand: {
+      primary: "#6610F2",
+      secondary: "#E6C229",
+      danger: "#D11149",
+      highlight: "#1A8FE3",
+      neutral: "#F9F8F8",
+      warning: "#F17105",
+    },
+  }
+  const theme = extendTheme({ colors })
   return (
     <ClerkProvider>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ClerkProvider>
   )
 }
